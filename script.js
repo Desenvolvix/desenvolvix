214866aa4083e56172c10fd8d1807974d2f9c93f
Mateus Santiago <mateussantiagomm@gmail.com>
2023-08-15 00:24:43 -0300

    att

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
   const telInput = document.getElementById("telContact");
 
-  formatPhoneNumber(telInput.value);
-
   telInput.addEventListener("input", function () {
-    formatPhoneNumber(this.value);
+    this.value = formatPhoneNumber(this.value);
   });
 
   function formatPhoneNumber(phoneNumber) {
     const cleanedPhoneNumber = phoneNumber.replace(/\D/g, "");
+    let formattedPhoneNumber = "";
 
     if (cleanedPhoneNumber.length <= 2) {
-      telInput.value = cleanedPhoneNumber;
+      formattedPhoneNumber = cleanedPhoneNumber;
     } else if (cleanedPhoneNumber.length <= 6) {
-      telInput.value = `(${cleanedPhoneNumber.slice(
+      formattedPhoneNumber = `(${cleanedPhoneNumber.slice(
         0,
         2
       )}) ${cleanedPhoneNumber.slice(2)}`;
     } else if (cleanedPhoneNumber.length <= 10) {
-      telInput.value = `(${cleanedPhoneNumber.slice(
+      formattedPhoneNumber = `(${cleanedPhoneNumber.slice(
         0,
         2
       )}) ${cleanedPhoneNumber.slice(2, 6)}-${cleanedPhoneNumber.slice(6)}`;
     } else {
-      telInput.value = `(${cleanedPhoneNumber.slice(
+      formattedPhoneNumber = `(${cleanedPhoneNumber.slice(
         0,
         2
       )}) ${cleanedPhoneNumber.slice(2, 7)}-${cleanedPhoneNumber.slice(7, 11)}`;
     }
+
+    return formattedPhoneNumber;
   }
+
+  telInput.value = formatPhoneNumber(telInput.value);
 });
